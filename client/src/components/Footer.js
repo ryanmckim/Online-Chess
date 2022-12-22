aaa8bdea61b58f9d133d30c70cf1ee86d7c0525e
CedricPulmano <pulmano.cedric@gmail.com>
2022-12-21 19:35:59 -0800

    Removed useless files, added basic skeleton

---
+++ client/src/components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <h1>Made by Cedric, Gordon, Malcolm, Sean, and Ryan</h1>;
+};
+
+export default Footer;
---
+++ client/src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <h1>Chess</h1>;
+};
+
+export default Header;
